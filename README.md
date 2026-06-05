bb87aad172d552500c93b5fae11fdf828c074554
Suraj <surajkushwaha88813@gmail.com>
2026-06-05 12:53:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Student Grade Calculator
+
+A simple Python project that calculates student grades based on marks.
+
+## Features
+- Takes marks as input
+- Calculates grade
+- Displays result
+
+## Technology Used
+- Python
---
+++ grade_calculator.py
@@ -0,0 +1,14 @@
+marks = float(input("Enter your marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 80:
+    grade = "B"
+elif marks >= 70:
+    grade = "C"
+elif marks >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Your Grade is:", grade)
